3e5cb660a7864a6fbe57bf581de4694a417202f3
EpicMaDneSss <regnier.david.pro@gmail.com>
2023-08-03 17:21:56 +0200

    REDUX TOOLKIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Form from "./components/Form";
+import PicCard from "./components/PicCard";
+import { useDispatch, useSelector } from "react-redux";
+import { setPicturesData } from "./feature/pictures.slice";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const picsData = useSelector((state) => state.pictures.pictures);
+
+  const getPictures = () => {
+    axios
+      .get("http://localhost:5000/pictures")
+      .then((res) => dispatch(setPicturesData(res.data)));
+  };
+
+  useEffect(() => {
+    getPictures();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>NFT Gallery</h1>
+      <Form getPictures={getPictures} />
+      <div className="cards-container">
+        {picsData?.map((pic, index) => (
+          <PicCard key={index} pic={pic} />
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
